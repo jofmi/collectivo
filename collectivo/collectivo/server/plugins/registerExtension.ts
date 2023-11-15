47d18e5f1868c634a24b23dd451115e58543022f
Joël <57440945+jofmi@users.noreply.github.com>
2023-11-15 15:05:45 +0100

    Enable messages migrations

--- collectivo/collectivo/server/plugins/registerExtension.ts
+++ collectivo/collectivo/server/plugins/registerExtension.ts
@@ -4,6 +4,7 @@ import m001_extensions from "../migrations/001_extensions";
 import m002_settings from "../migrations/002_settings";
 import m003_tags from "../migrations/003_tags";
 import m004_tiles from "../migrations/004_tiles";
+import m005_messages from "../migrations/005_messages";
 
 import exampleDataFn from "../exampleData/exampleData";
 
@@ -12,7 +13,13 @@ export default defineNitroPlugin((nitroApp) => {
   registerExtension({
     name: "collectivo",
     version: pkg.version,
-    migrations: [m001_extensions, m002_settings, m003_tags, m004_tiles],
+    migrations: [
+      m001_extensions,
+      m002_settings,
+      m003_tags,
+      m004_tiles,
+      m005_messages,
+    ],
     exampleDataFn: exampleDataFn,
   });
 });

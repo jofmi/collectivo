9dc385aafb89c061c2faae7fe49068c74459b41d
Joël <57440945+jofmi@users.noreply.github.com>
2023-12-06 10:46:26 +0100

    feat: add forms page

---
+++ collectivo/collectivo/pages/forms/[formName].vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const route = useRoute();
+const formName = route.params.formName;
+</script>
+
+<template>FORM {{ formName }}</template>
+
+<style lang="scss" scoped></style>

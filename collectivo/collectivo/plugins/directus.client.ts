d092a5aa078f4d6a158abc293062c2f8c6454ccf
Joël <57440945+jofmi@users.noreply.github.com>
2024-01-05 11:51:29 +0100

    feat: safe error page

--- collectivo/collectivo/plugins/directus.client.ts
+++ collectivo/collectivo/plugins/directus.client.ts
@@ -4,7 +4,28 @@ import { createDirectus, authentication, rest } from "@directus/sdk";
 export default defineNuxtPlugin({
   name: "directus-client",
   enforce: "pre",
-  async setup() {
+  async setup(nuxtApp) {
+    // nuxtApp.hook("vue:error", (..._args) => {
+    //   console.log("vue:error");
+    //   // if (process.client) {
+    //   //   console.log(..._args)
+    //   // }
+    // });
+
+    // nuxtApp.hook("app:error", (..._args) => {
+    //   console.log("app:error");
+    //   // if (process.client) {
+    //   //   console.log(..._args)
+    //   // }
+    // });
+
+    // nuxtApp.vueApp.config.errorHandler = (..._args) => {
+    //   console.log("global error handler");
+    //   // if (process.client) {
+    //   //   console.log(..._args)
+    //   // }
+    // };
+
     let directus;
     const runtimeConfig = useRuntimeConfig();
     const user = useCollectivoUser();
@@ -17,7 +38,11 @@ export default defineNuxtPlugin({
         .with(authentication("cookie", { credentials: "include" }))
         .with(rest({ credentials: "include" }));
     } catch (e) {
-      throw new Error("Environment variable NUXT_PUBLIC_DIRECTUS_URL invalid");
+      throw createError({
+        statusMessage: "Environment variable NUXT_PUBLIC_DIRECTUS_URL invalid",
+        statusCode: 500,
+        fatal: true,
+      });
     }
 
     // Try to refresh token and set user to authenticated if successful
@@ -26,8 +51,15 @@ export default defineNuxtPlugin({
       user.value.isAuthenticated = true;
     } catch (e: any) {
       // If error is not auth-related, throw error
-      if (![400, 401, 403].includes(e.response.status)) {
-        throw new Error("Cannot reach backend server (directus)");
+      if (![400, 401, 403].includes("response" in e && e.response.status)) {
+        throw createError({
+          statusMessage: "Cannot reach backend server (directus)",
+          statusCode: 500,
+          fatal: true,
+        });
+      } else {
+        // If error is auth-related, redirect to keycloak
+        console.error("Error in directus.client.ts", e);
       }
     }
 

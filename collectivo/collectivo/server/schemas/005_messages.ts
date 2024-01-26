1101a529b0db081b8221f8ab0ad0314d8dee1e58
Christopher <christopher.guy.wolf@protonmail.com>
2024-01-26 13:57:02 +0100

    Update 005_messages.ts
    Draft flow to expand campaign to individual messages

--- collectivo/collectivo/server/schemas/005_messages.ts
+++ collectivo/collectivo/server/schemas/005_messages.ts
@@ -190,3 +190,59 @@ const operation = {
     to: ["{{ recipient.email }}"],
   },
 };
+
+// @ts-ignore
+const flowToExpandCampaignToIndividualMessages = {
+  id: "067867f6-da20-49f9-a7a7-cd6bf1d6025f",
+  name: "messages_create_messages_from_campaign",
+  icon: "conveyor_belt",
+  color: null,
+  description: "Creates the individual messages to execute a campaign",
+  status: "active",
+  trigger: "event",
+  accountability: "all",
+  options: {
+    type: "action",
+    scope: ["items.create"],
+    collections: ["messages_campaigns"],
+  },
+  operation: "56cc8889-4ae4-40c6-8417-b2b74c7bac49",
+  date_created: "2024-01-07T11:26:02.827Z",
+  user_created: "a0cf509f-8dd8-47a5-8bbe-d439e0cafc7b",
+    {
+      id: "56cc8889-4ae4-40c6-8417-b2b74c7bac49",
+      name: "messages_expaned_campaign_to_messages",
+      key: "messages_expaned_campaign_to_messages",
+      type: "exec",
+      position_x: 19,
+      position_y: 1,
+      options: {
+        code: 'module.exports = async function(data) {\n    campaign = data["$trigger"].payload;\n\trecordsToCreate = [];\n    for (i in data["$trigger"].payload.recipients.create) {\n        recipient = data["$trigger"].payload.recipients.create[i]\n        recordsToCreate.push({\n            "messages_campaign": data["$trigger"].key,\n            "recipient": recipient.directus_users_id.id\n        });\n    }\n\treturn {recordsToCreate};\n}',
+      },
+      resolve: "d458a922-7f98-448d-b998-aeb29793770b",
+      reject: null,
+      flow: "067867f6-da20-49f9-a7a7-cd6bf1d6025f",
+      date_created: "2024-01-07T11:28:28.142Z",
+      user_created: "a0cf509f-8dd8-47a5-8bbe-d439e0cafc7b",
+    },
+    {
+      id: "d458a922-7f98-448d-b998-aeb29793770b",
+      name: "messages_store_individual_messages_in_records",
+      key: "messages_store_individual_messages_in_records",
+      type: "item-create",
+      position_x: 37,
+      position_y: 1,
+      options: {
+        collection: "messages_records",
+        emitEvents: true,
+        payload: "{{$last.recordsToCreate}}",
+      },
+      resolve: null,
+      reject: null,
+      flow: "067867f6-da20-49f9-a7a7-cd6bf1d6025f",
+      date_created: "2024-01-07T11:28:28.086Z",
+      user_created: "a0cf509f-8dd8-47a5-8bbe-d439e0cafc7b",
+    },
+  ],
+};
+

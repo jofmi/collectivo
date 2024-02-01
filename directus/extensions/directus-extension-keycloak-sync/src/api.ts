97bce6df62e9e59d854c3ced7fd9042735f97eb5
Aaron <aaron@nanu-c.org>
2024-02-01 10:52:05 +0100

    feat: sync also on create+delete

--- directus/extensions/directus-extension-keycloak-sync/src/api.ts
+++ directus/extensions/directus-extension-keycloak-sync/src/api.ts
@@ -13,15 +13,6 @@ export default defineOperationApi<Options>({
 	id: 'keycloak_sync',
 	handler: async ({ text }, context) => {
 
-		// check if we have all the data we need
-		if (!context.data.read_user_data) {
-			console.log("No user data");
-			throw new Error("No user data");
-		}
-		if (!context.data.read_user_data.email) {
-			console.log("No user email");
-			throw new Error("No user email");
-		}
 		if (!context.env.AUTH_KEYCLOAK_URL) {
 			console.log("No keycloak url");
 			throw new Error("No keycloak url");
@@ -38,7 +29,31 @@ export default defineOperationApi<Options>({
 			console.log("No keycloak secret");
 			throw new Error("No keycloak secret");
 		}
-		const user = context.data.read_user_data;
+		let user = context.data.read_user_data;
+		let is_delete = false;
+
+		// check if we have all the data we need
+		if (!user || user.name === "DirectusError") {
+			console.log("No user data, check if user was created/deleted", JSON.stringify(context.data.$trigger.event));
+			// user not found but maybe it was created/deleted
+			if ( context.data.$trigger.event === "users.create")
+			{
+				user = context.data.$trigger.payload;
+				user.id = context.data.$trigger.key;
+			} else if ( context.data.$trigger.event === "users.delete")
+			{
+				console.log("User was deleted", JSON.stringify(context.data.$trigger));
+
+				user.id = context.data.$trigger.payload;
+				is_delete = true;
+			}
+		}
+		if (!is_delete && !user.email) {
+			console.log("No user email");
+			throw new Error("No user email");
+		}
+
+
 
 		const keycloak = new KcAdminClient({
 			baseUrl: context.env.AUTH_KEYCLOAK_URL,
@@ -51,6 +66,15 @@ export default defineOperationApi<Options>({
 				clientId: context.env.AUTH_KEYCLOAK_CLIENT_ID,
 				clientSecret: context.env.AUTH_KEYCLOAK_CLIENT_SECRET,
 			});
+			if (is_delete)
+			{
+				// delete user
+				const keycloak_user = await keycloak.users.find({ "directus_uuid": user.id });
+				if (keycloak_user && keycloak_user.length>0 ){
+					await keycloak.users.del({ id: keycloak_user[0].id });
+				}
+				return {};
+			}
 			// check if user exists
 			let new_user = false;
 			if (!user.keycloak_uuid) {
@@ -66,10 +90,20 @@ export default defineOperationApi<Options>({
 						firstName: user.first_name,
 						lastName: user.last_name,
 						enabled: true,
+						attributes: {
+							"directus_uuid": user.id,
+						},
 					});
 					new_user = true;
 					if (keycloak_user) {
 						response["keycloak_uuid"] = keycloak_user.id;
+						user.keycloak_uuid = keycloak_user.id;
+						if (!user.collectivo_tags) {
+							user.collectivo_tags = [];
+						}
+						if (!user.role) {
+							user.role = "0";
+						}
 					}
 				}
 			}
@@ -88,6 +122,9 @@ export default defineOperationApi<Options>({
 							firstName: user.first_name,
 							lastName: user.last_name,
 							enabled: true,
+							attributes: {
+								"directus_uuid": user.id,
+							},
 						};
 						if (keycloak_user.email !== user.email){
 							// email changed, lets set it to unverified
@@ -106,7 +143,12 @@ export default defineOperationApi<Options>({
 					const keycloak_user_groups = await keycloak.users.listGroups({ id: user.keycloak_uuid });
 					// map directus tag id to name
 					const user_tags_names = user.collectivo_tags.map((id) => context.data.user_tags.find((tag) => tag.id === id)?.name.replace(/ /g, "_").toLowerCase());
-					const user_roles_name = context.data.user_roles.find((role) => role.id === user.role)?.name.replace(/ /g, "_").toLowerCase();
+					let user_roles = []
+					if (context.data.user_roles !== undefined) {
+						user_roles = context.data.user_roles;
+					}
+
+					const user_roles_name = user_roles.find((role) => role.id === user.role)?.name.replace(/ /g, "_").toLowerCase();
 					// combine to one list 
 					const full_groups_list = [...user_tags_names, user_roles_name];
 
@@ -124,8 +166,11 @@ export default defineOperationApi<Options>({
 						}
 						let keycloak_groups = await keycloak.groups.find();
 						for (const groupName of keycloak_user_group_ids_to_add) {
-							console.log(`Add user ${user.email} to group ${groupName}`);
 							// check if group exists
+							if (groupName === "undefined" || groupName === undefined) {
+								continue;
+							}
+							console.log(`Add user ${user.email} to group ${groupName}`);
 							let keycloak_group = keycloak_groups.find((group) => group.name === groupName);
 							if (keycloak_group === undefined) {
 								console.log("No keycloak group, lets create one ", groupName);

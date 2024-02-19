cd0cabafb1b65ac0677c89a243335dd244100593
Joël <57440945+jofmi@users.noreply.github.com>
2024-02-19 08:29:20 +0100

    feat: use select for date

--- collectivo/collectivo/components/collectivo/form/Date.vue
+++ collectivo/collectivo/components/collectivo/form/Date.vue
@@ -19,6 +19,10 @@ const day = ref();
 const month = ref();
 const year = ref();
 
+const days = Array.from({ length: 31 }, (_, i) =>
+  (i + 1).toString().padStart(2, "0"),
+);
+
 const months = [
   "January",
   "February",
@@ -33,6 +37,10 @@ const months = [
   "November",
   "December",
 ];
+
+const years = Array.from({ length: 150 }, (_, i) =>
+  (new Date().getFullYear() - i).toString(),
+);
 
 if (props.modelValue) {
   date.value = new Date(props.modelValue);
@@ -49,23 +57,20 @@ watch(
 );
 
 watch([day, month, year], ([day, month, year]) => {
-  if (day && month && year) {
-    const thisYear = new Date().getFullYear();
-
-    if (
-      props.birthdate &&
-      (Number(year) < thisYear - 150 || Number(year) > thisYear)
-    ) {
-      date.value = new Date("invalid");
-    } else {
-      date.value = new Date(`${year}-${month}-${day} UTC`);
-    }
-  }
+  if (day && month && year)
+    date.value = new Date(`${year}-${month}-${day} UTC`);
 });
 </script>
 
 <template>
   <div class="flex flex-row gap-2">
+    <USelectMenu
+      v-model="day"
+      :options="days"
+      class="w-1/3"
+      :placeholder="t('Day')"
+    />
+
     <USelectMenu
       v-model="month"
       :options="months"
@@ -79,8 +84,13 @@ watch([day, month, year], ([day, month, year]) => {
         {{ t(option) }}
       </template>
     </USelectMenu>
-    <UInput v-model="day" class="w-1/3" :placeholder="t('Day')" />
-    <UInput v-model="year" class="w-1/3" :placeholder="t('Year')" />
+
+    <USelectMenu
+      v-model="year"
+      :options="years"
+      class="w-1/3"
+      :placeholder="t('Year')"
+    />
   </div>
 </template>
 

89ca1562f1a1195773a66fb025a70900e3469f2f
Joël <57440945+jofmi@users.noreply.github.com>
2024-04-12 14:02:58 +0200

    fix: dont use tailwind class

--- collectivo/collectivo/components/collectivo/Container.vue
+++ collectivo/collectivo/components/collectivo/Container.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container">
+  <div class="cv-container">
     <slot></slot>
   </div>
 </template>
 
 <style scoped lang="scss">
-.container {
+.cv-container {
   @apply rounded-xl p-[25px] mb-5 bg-white;
   box-shadow: 0px 6px 48px 0px rgba(220, 226, 239, 0.5);
 }
